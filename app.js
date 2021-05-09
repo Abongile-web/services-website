f82cfa5bbabcdd3e526a60e9fb7d494850081477
Abongile William <abongilewilliam@gmail.com>
2021-05-09 21:10:33 +0200

    projects section responsive

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 //menu button on mobile
 
 document.querySelector('.menu').addEventListener('click', () => {
-    console.log('menu clicked');
     document.querySelector('.navigation ul').classList.toggle('show');
+
+    let icon = document.querySelector('.menu i');
+
+    if (icon.classList == 'fa fa-bars') {
+        icon.className = 'fa fa-times';
+    } else {
+        icon.className = 'fa fa-bars';
+    }
 })
